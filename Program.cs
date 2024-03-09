36f8b5da256005611479fa6be8894ebe838da915
valentina-cano-naranjo <159857592+valentina-cano-naranjo@users.noreply.github.com>
2024-03-08 21:59:30 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,25 @@
 ﻿// Velocidades de los trenes en km/h
-int velocidadPrimerTren = 80;
-int velocidadSegundoTren = 100;
+int vpt= 80;
+int vst = 100;
 
 // Tiempo en horas cuando sale el segundo tren
-int horaSalidaSegundoTren = 11;
-int minutosSalidaSegundoTren = 0;
+int hsst = 11;
+int msst= 0;
 
 // Tiempo en horas cuando sale el primer tren
-int horaSalidaPrimerTren = 10;
-int minutosSalidaPrimerTren = 0;
+int hspt = 10;
+int mspt = 0;
 
 // Convertir todo a minutos
-int tiempoPrimerTren = horaSalidaPrimerTren * 60 + minutosSalidaPrimerTren;
-int tiempoSegundoTren = horaSalidaSegundoTren * 60 + minutosSalidaSegundoTren;
+int tpt = hspt * 60 + mspt;
+int tst= hsst * 60 + msst;
 
 // Calcular el tiempo que el segundo tren tarda en alcanzar al primer tren
-int tiempoDeAlcance = (int)(((double)tiempoSegundoTren - tiempoPrimerTren) / (velocidadSegundoTren - velocidadPrimerTren) * 60);
+int ta = (int)(((double)tst - tpt) / (vst - vpt) * 60);
 
 // Calcular la hora de alcance
-int horaDeAlcance = horaSalidaSegundoTren + tiempoDeAlcance / 60;
-int minutosDeAlcance = tiempoDeAlcance % 60;
+int ha = hsst + ta / 60;
+int ma = ta % 60;
 
-Console.WriteLine($"El segundo tren alcanzará al primer tren a las {horaDeAlcance}:{minutosDeAlcance:D2}.");
+Console.WriteLine($"El segundo tren alcanzará al primer tren a las {ha}:{ma :D2}.");
 
